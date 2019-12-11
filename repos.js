d696d09657971426e061d21170ecd4e99864b0fb
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-12-11 14:03:01 -0500

    Removing use strict

--- repos.js
+++ repos.js
@@ -1,41 +1,53 @@
-'use strict';
 /**
  * Fetch user repos
  */
 const getRepos = function() {
   fetch('https://api.github.com/users/andreacardybailey/repos')
     .then(response => response.json())
+    // 👇 You MUST work with the data HERE 👇
     .then(jsonData => {
       extractData(jsonData);
     })
-    .catch(error => {
-      console.log(`There was an error fetching your data: ${error.message}.`);
-    });
+    .catch(error => console.log(error));
 };
 
-const extractData = function(jsonData) {
-  jsonData.forEach(repo => {
+/**
+ * Extract data to be used on page
+ * @param data - the JSON data
+ */
+const extractData = function(data){
+  data.forEach(repo => {
     let {
       name,
       html_url,
       created_at,
-      description,
+      description
     } = repo;
+    
     let dateCreated = new Date(created_at);
-    $('.repos').append(createTemplate(name, html_url, description, dateCreated));
+    $('.repos').append(createTemplate(name, html_url, dateCreated, description));
   });
 };
 
-const createTemplate = function(name, html_url, description, dateCreated) {
-  let template = 
-    `<section>
-      <h2><a href="${html_url}" target="_blank">${name}</a></h2>
-      <ul>
-        <li>Description: ${description}</li>
-        <li>Date created: ${dateCreated.getMonth()}/${dateCreated.getDate()}/${dateCreated.getFullYear()}</li>
-      </ul>
-    </section>
-    `;
+/**
+ * Create HTML template for each result
+ * @param repo_name
+ * @param url
+ * @param created_at
+ * @param decription
+ */
+const createTemplate = function(repo_name, url, date, description) {
+  let template = `
+  <section>
+    <h2><a href="${url}">${repo_name}</a></h2>
+    <ul>
+      <li>Description: ${description}</li>
+      <li>
+        Date created: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}
+      </li>
+    </ul>
+  </section>
+  `;
   return template;
 };
 
